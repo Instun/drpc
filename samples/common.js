7b9ffc5d1bee8638409278d427ee1c2c80227fde
xicilion <lion@9465.net>
2024-09-17 17:47:05 +0800

    v2.3.0, support timeout.

--- samples/common.js
+++ samples/common.js
@@ -36,17 +36,30 @@ exports.handler = rpc.handler({
             }
         }
     },
-    test_func: test_func
+    test_func: test_func,
+    timeout: async function (v1, v2) {
+        await sleep(20000);
+        return v1 + v2;
+    }
 });
 
 exports.test = async function (conn) {
-    const remoting = rpc.open(conn);
+    const remoting = rpc.open(conn, {
+        timeout: 3000
+    });
 
     console.log(`remoting.test(1, 2) === ${await remoting.test(1, 2)}`)
     console.log(`remoting.test.test1(2, 3) === ${await remoting.test.test1(2, 3)}`)
     console.log(`remoting.test.test1.test2(3, 4) === ${await remoting.test.test1.test2(3, 4)}`)
     console.log(`remoting.test1.test2(4, 5) === ${await remoting.test1.test2(4, 5)}`)
     console.log(`remoting.test1.test3.test4(5, 6) === ${await remoting.test1.test3.test4(5, 6)}`)
+
+    try {
+        console.log(`remoting.timeout(5, 6) === ${await remoting.timeout(5, 6)}`)
+    } catch (e) {
+        console.log(e);
+    }
+
     // console.log(`remoting.test2.test3(3, 4) === ${await remoting.test2.test3(4, 5)}`)
     // console.log(`remoting.test_func.test6(3, 4) === ${await remoting.test_func.test6(4, 5)}`)
 
